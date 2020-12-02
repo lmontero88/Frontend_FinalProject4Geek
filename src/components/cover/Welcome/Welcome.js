be0c7a1e67bbc6c78365f5f2dff12633d6da4a9d
Guaaan <72053761+Guaaan@users.noreply.github.com>
2020-12-02 17:05:08 -0300

    agregué colores al login y una descripción breve

--- src/components/cover/Welcome/Welcome.js
+++ src/components/cover/Welcome/Welcome.js
@@ -4,6 +4,8 @@ import './Welcome.scss';
 const Welcome = () => {
   return (
     <div>
+      <h1>Bienvenido a SPORTMATCH<i class="fas fa-running"></i></h1>
+      <h2>Donde encontrar con quien entrenar <i class="fas fa-basketball-ball"></i></h2>
 
     </div>
   )
