53d7b7d0b287f9007714b7d2fb1b6849e83891ec
Guaaan <72053761+Guaaan@users.noreply.github.com>
2020-12-14 12:12:17 -0300

    cambios

--- src/components/commons/Post/Post.js
+++ src/components/commons/Post/Post.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './Post.scss';
 
-function Post() {
+function Post(imagen,titulo,contenido,boton) {
     return (
             <div className="card carta">
-              <img src="https://images.pexels.com/photos/358042/pexels-photo-358042.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" className="card-img-top" alt="..."/>
+              <img src={`${imagen}`} className="card-img-top" alt="..."/>
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <h5 className="card-title">{`${titulo}`}</h5>
+                <p className="card-text">{`${contenido}`}</p>
+                <a href="#" className="btn btn-primary">{`${boton}`}</a>
               </div>
             </div>
     )
